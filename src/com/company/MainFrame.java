34de71577e1ab45572556e3233477f1e26f74a33
chenchianstudent <105021007@live.asia.edu.tw>
2017-10-17 11:50:09 +0800

    02

--- src/com/company/MainFrame.java
+++ src/com/company/MainFrame.java
@@ -7,37 +7,51 @@ import java.awt.event.ActionListener;
 
 
 public class MainFrame extends JFrame {
-    private JLabel lab1=new JLabel("ID:");
-    private JLabel lab2=new JLabel("Password:");
-    private JTextField tf1=new JTextField();
-    private JTextField tf2=new JTextField();
-    private JButton bt1=new JButton("Exit");
-    private JButton bt2=new JButton("Enter");
+    private JLabel lab1 = new JLabel("ID:");
+    private JLabel lab2 = new JLabel("Password:");
+    private JTextField tf1 = new JTextField();
+    private JPasswordField tf2 = new JPasswordField();
+    private JButton bt1 = new JButton("Exit");
+    private JButton bt2 = new JButton("Enter");
     private Container cp;
+    private Dimension dim=Toolkit.getDefaultToolkit().getScreenSize();
+    private int frmW=300,frmH=150,screenW,screenH;
+    public MainFrame() {
+        init();
+    }
 
+    private void init() {
+        screenW=dim.width;
+        screenH=dim.height;
+        this.setBounds(screenW/2-frmW/2, screenH/2-frmH/2, frmW, frmH);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        cp = this.getContentPane();
+        cp.setLayout(new GridLayout(3, 2, 3, 1));
+        lab1.setHorizontalAlignment(JLabel.RIGHT);
+        lab2.setHorizontalAlignment(JLabel.RIGHT);
+        cp.add(lab1);
+        cp.add(tf1);
+        cp.add(lab2);
+        cp.add(tf2);
+        cp.add(bt1);
+        cp.add(bt2);
+        bt1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        bt2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+              if(tf1.getText().equals("poi")&&new String(tf2.getPassword()).equals("123456")){
+                MainFrame2 mf2=new MainFrame2();
+                mf2.setVisible(true);
+              } else{
+               JOptionPane.showMessageDialog(null,"error");
+              }
+            }
+        });
 
-public MainFrame(){
-    init();
-}
-private void init(){
- this.setBounds(100,100,300,150);
- this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    cp = this.getContentPane();
-    cp.setLayout(new GridLayout(3,2,3,1));
-    lab1.setHorizontalAlignment(JLabel.RIGHT);
-    lab2.setHorizontalAlignment(JLabel.RIGHT);
-    cp.add(lab1);
-    cp.add(tf1);
-    cp.add(lab2);
-    cp.add(tf2);
-    cp.add(bt1);
-    cp.add(bt2);
-    bt1.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-           System.exit(0);
-        }
-    });
-
-}
+    }
 }
---
+++ src/com/company/MainFrame2.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import javax.swing.*;
+
+public class MainFrame2 extends JFrame {
+   public MainFrame2(){
+      init();
+   }
+   private void init(){
+       this.setBounds(100,100,400,600);
+       this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+   }
+}
